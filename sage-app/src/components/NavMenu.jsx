c95dc9b7345b9aa95073e6ac964f198154855579
DmXavior <devonmayaxavior@gmail.com>
2024-08-29 23:12:56 +0800

    Added Landing to NavMenu

--- sage-app/src/components/NavMenu.jsx
+++ sage-app/src/components/NavMenu.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaLightbulb, FaComment, FaRegHeart, FaUser } from 'react-icons/fa';
+import { FaRocket, FaHome, FaLightbulb, FaComment, FaRegHeart, FaUser } from 'react-icons/fa';
 
 
 function NavMenu() {
   return (
     <nav className="nav-container p-4 md:relative md:top-0 md:left-0 md:w-auto fixed bottom-0 left-0 w-full">
       <ul className="flex justify-between md:justify-center space-x-4">
+      <li className='flex-1 text-center'>
+          <Link to="/">
+            <FaRocket className="md:hidden mx-auto" size={24} color="#defcf9" />
+            <span className="hidden md:inline">Landing</span>
+          </Link>
+        </li>
         <li className='flex-1 text-center'>
           <Link to="/homepage">
             <FaHome className="md:hidden mx-auto" size={24} color="#defcf9" />
